7907054c10acdfcf0e5c49163996664a299dbf93
Assem <koffka.rd@gmail.com>
2021-05-04 01:34:33 +0600

    Future Talants
    Future Talants

---
+++ Assets/js/script.js
@@ -0,0 +1,9 @@
+$('#menu-btn').on('click', function(){
+	$('.nav-menu').toggleClass("active");
+});
+$('#media-btn').on('click', function(){
+	$('.media-icons').toggleClass("active");
+});
+$('#media-btn2').on('click', function(){
+	$('#media-icons2').toggleClass("active");
+});
